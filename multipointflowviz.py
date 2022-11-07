522178ae09ec17428f57385dce4f6fa91d6e3c96
mirarzf <mira.razaf@gmail.com>
2022-11-07 13:23:03 +0900

    Updated multipointflowviz

--- multipointflowviz.py
+++ multipointflowviz.py
@@ -9,28 +9,39 @@ import numpy as np
 import pandas as pd
 import cv2 as cv
 import torch
-from PIL import Image
 
 from utils import flow_viz, annot_viz
 
 from raft import RAFT
 from utils.utils import InputPadder
 
-### Folders 
+# torch.cuda.empty_cache()
 
+### Folders 
 outputfolder = ".\\results"
+model_folders = "C:\\Users\\hvrl\\Documents\\RAFT-master\\models"
 
 ### Main code 
 DEVICE = 'cuda'
 
+def load_image(img, destsize): 
+    img = cv.resize(img, destsize)
+    img = torch.from_numpy(img).permute(2, 0, 1).float()
+    return img[None].to(DEVICE)
+
+def unscaledCoordlist(coordlist, scale=1): 
+    newcoordlist = [(e[0]/scale, e[1]/scale) for e in coordlist]
+    return newcoordlist
+
 def showAnnotatedPointsFlow(args): 
 
+    # Set the arguments from the parser 
     annotatedpoints = args.dataset
     annotatedpoints = "centerpointstest.csv"
 
-    video_flow_folder = "C:\\Users\\hvrl\\Documents\\data\\KU\\of" 
-    video_folder = "C:\\Users\\hvrl\\Documents\\data\\KU\\videos"
-    model_folders = "C:\\Users\\hvrl\\Documents\\RAFT-master\\models"
+    video_folder = args.videofolder
+
+    scale = args.scale
 
     # Initialize RAFT model 
     model = torch.nn.DataParallel(RAFT(args))
@@ -40,104 +51,100 @@ def showAnnotatedPointsFlow(args):
     model.to(DEVICE)
     model.eval()
 
-    # Read the annotated data 
-    apdf = pd.read_csv(annotatedpoints)
+    with torch.no_grad(): 
 
-    # Get all the video_ids 
-    video_id_list = apdf["video_id"].unique()
+        # Read the annotated data 
+        apdf = pd.read_csv(annotatedpoints)
 
-    # Work on one video_id at a time 
-    for video_id in video_id_list: 
-        partdf = apdf[apdf["video_id"] == video_id]
+        # Get all the video_ids 
+        video_id_list = apdf["video_id"].unique()
 
-        # Get the coordinates of the annotated points 
-        aplist = []
-        for index, row in partdf.iterrows(): 
-            j, i = row["x_coord"], row["y_coord"]
-            aplist.append((i,j))
+        # Work on one video_id at a time 
+        for video_id in video_id_list: 
+            partdf = apdf[apdf["video_id"] == video_id]
 
-        n = len(aplist)
-        randomcolors = [np.random.randint(256, size=3) for index in range(n)]
-        print(randomcolors)
+            # Read original videos for parameters of the writer 
+            origvidpath = os.path.join(video_folder, video_id + "_extract.mp4")
+            cap = cv.VideoCapture(origvidpath)
 
-        # Read original videos for parameters of the writer 
-        origvidpath = os.path.join(video_folder, video_id + "_extract.mp4")
-        cap = cv.VideoCapture(origvidpath)
+            # Define output video parameters 
+            outputname = "out_annotated_" + video_id + ".mp4" 
+            print(outputname)
+            fourcc = cv.VideoWriter_fourcc(*'mp4v')
+            fps = cap.get(cv.CAP_PROP_FPS)
+            frame_width = int(scale*cap.get(cv.CAP_PROP_FRAME_WIDTH)) 
+            frame_height = int(scale*cap.get(cv.CAP_PROP_FRAME_HEIGHT)) 
+            print(origvidpath, fps, frame_width, frame_height)
+            print(cap.get(cv.CAP_PROP_FRAME_WIDTH))
 
-        # Define output video parameters 
-        outputname = "out_annotated_" + video_id + ".mp4" 
-        print(outputname)
-        fourcc = cv.VideoWriter_fourcc(*'mp4v')
-        fps = cap.get(cv.CAP_PROP_FPS)
-        frame_width = currentflow.shape[1]
-        frame_height = currentflow.shape[0]
-        print(origvidpath, fps)
+            # Define output video writer 
+            # output = cv.VideoWriter(os.path.join(outputfolder, outputname), fourcc, fps, (frame_width*2, frame_height))
+            output = cv.VideoWriter(os.path.join(outputfolder, outputname), fourcc, fps, (frame_width*2, frame_height))
 
-        # Define output video writer 
-        output = cv.VideoWriter(os.path.join(outputfolder, outputname), fourcc, fps, (frame_width*2, frame_height))
+            # Get the coordinates of the annotated points 
+            aplist = []
+            for index, row in partdf.iterrows(): 
+                j, i = row["x_coord"], row["y_coord"]
+                j, i = int(j*scale), int(i*scale)
+                aplist.append((i,j))
 
-        # Capture the first two frames 
-        ret, frame1 = cap.read() 
-        ret, frame2 = cap.read()
+            n = len(aplist)
+            print(aplist)
+            randomcolors = [np.random.randint(256, size=3) for index in range(n)]
+            print("Il y a {n} points annotes dans la video {video_id}".format(n=n, video_id = video_id))
 
-        # Calculate the optical flow for first pair of frames 
+            # Capture the first two frames 
+            ret, beforeframe = cap.read() 
+            ret, currentframe = cap.read()
+
+            while ret and len(aplist) > 0: 
+                # Prep the frames for optical flow retrieving
+                beforeframe = load_image(beforeframe, (frame_width, frame_height))
+                currentframe = load_image(currentframe, (frame_width, frame_height))
+                padder = InputPadder(beforeframe.shape)
+                beforeframe, currentframe = padder.pad(beforeframe, currentframe)
+
+                # Retrieve the optical flow between beforeframe and currentframe 
+                flow_low, currentflow = model(beforeframe, currentframe, iters=20, test_mode=True)
+                
+                # Draw on the image 
+                currentframe = currentframe[0].permute(1,2,0).cpu().numpy() 
+                currentflow = currentflow[0].permute(1,2,0).cpu().detach().numpy()
+                unscaledaplist = unscaledCoordlist(aplist, scale)
+                frameimg = annot_viz.visualizePoint(currentframe.astype('uint8').copy() , unscaledaplist, color=randomcolors, scale=scale) # multiple points 
+                flowimg = flow_viz.flow_to_image(currentflow, convert_to_bgr=True)
+                flowimg = annot_viz.visualizePoint(flowimg, unscaledaplist, color=randomcolors, scale=scale) # multiple points 
+                concatenation = cv.hconcat([frameimg, flowimg])
+                # cv.imshow("result", concatenation)
+                # cv.waitKey(0)
+                output.write(concatenation)
+
+                # Get the new vector of comparison 
+                aplist, inFrameList = annot_viz.calculateNewPosition(aplist, currentflow) # multiple points 
+                # Update the color list to only keep the points in frame 
+                updaterandomcolors = []
+                for bool, color in zip(inFrameList, randomcolors): 
+                    if bool: 
+                        updaterandomcolors.append(color)
+                randomcolors = updaterandomcolors
+                
+                print("Points etant encore in frame :", len(aplist), len(randomcolors))
+
+                # Set new currentframe 
+                beforeframe = currentframe
+                ret, currentframe = cap.read()
         
-        framenames = [f for f in os.listdir(video_flow_folder) if video_id in f and "mirrored" not in f]
-        framenames.sort()
-
-        flowfiles = [os.path.join(video_flow_folder, f) for f in framenames]
-
-        # Retrieve the optical flow of the first annotated frame 
-        currentofname = os.path.join(video_flow_folder, partdf["video_frame_id"].iloc[0] + ".npy")
-        currentframename = os.path.join(video_frames_folder, partdf["video_frame_id"].iloc[0] + ".png")
-        currentflow = np.load(currentofname)
-        currentframe = cv.imread(currentframename)
-        
-        # Compare for each frame in the video the optical flow of the annotated flow to the one of the annotated point 
-        currentframenb = partdf["frame_number"].iloc[0]
-        currentofname = partdf["video_frame_id"].iloc[0]
-        while currentframenb < len(framenames) and len(aplist) > 0: 
-            # Draw on the image 
-            annot_viz.visualizePoint(currentframe, aplist, color=randomcolors) # multiple points 
-            flowimg = flow_viz.flow_to_image(currentflow, convert_to_bgr=True)
-            annot_viz.visualizePoint(flowimg, aplist, color=randomcolors) # multiple points 
-            concatenation = cv.hconcat([currentframe, flowimg])
-            output.write(concatenation)
-
-            # Determine the next optical flow vector of comparison
-            currentframenb += 1
-
-            # Get the new frame of optical flow 
-            newofnamelist = os.path.basename(os.path.splitext(currentofname)[0]).split("_")[:-1]
-            newofnamelist.append(str(currentframenb))
-            newofname = annot_viz.reconstructFilenameFromList(newofnamelist)
-            currentofname = os.path.join(video_flow_folder, newofname + ".npy")
-            currentframename = os.path.join(video_frames_folder, newofname + ".png")
-
-            # Set new currentflow and new currentframe 
-            currentflow = np.load(currentofname)
-            currentframe = cv.imread(currentframename)
-
-            # Get the new vector of comparison 
-            aplist, inFrameList = annot_viz.calculateNewPosition(aplist, currentflow) # multiple point 
-            # Update the color list to only keep the points in frame 
-            updaterandomcolors = []
-            for bool, color in zip(inFrameList, randomcolors): 
-                if bool: 
-                    updaterandomcolors.append(color)
-            randomcolors = updaterandomcolors
-            
-            print("Points etant encore in frame :", len(aplist), len(randomcolors))
-        
-        cap.release()
-        output.release() 
+            cap.release()
+            output.release() 
 
         print(video_id, len(aplist))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', default='raft-sintel.pth', help="restore checkpoint")
-    parser.add_argument('--dataset', default='C:\\Users\\hvrl\\Documents\\data\\KU\\centerpoints.csv', help="dataset for evaluation")
+    parser.add_argument('--dataset', default='C:\\Users\\hvrl\\Documents\\data\\KU\\centerpoints.csv', help="CSV file with annotated points")
+    parser.add_argument('--videofolder', '-vf', default='C:\\Users\\hvrl\\Documents\\data\\KU\\videos', help="folder containig the annotated videos")
+    parser.add_argument('--scale', default=0.5, type=float, help="scale to resize the video frames. Default: 0.5")
     parser.add_argument('--small', action='store_true', help='use small model')
     parser.add_argument('--mixed_precision', action='store_true', help='use mixed precision')
     parser.add_argument('--alternate_corr', action='store_true', help='use efficent correlation implementation')
---
+++ raftcore/utils/annot_viz.py
@@ -0,0 +1,45 @@
+import cv2 as cv
+
+### Utility functions 
+
+def reconstructFilenameFromList(name_elements): 
+    filename = name_elements[0]
+    for e in name_elements[1:]: 
+        filename += "_"
+        filename += e
+    return filename
+
+def visualizePoint(img, coordlist, color=[(0,0,255)], scale = 1): 
+    '''
+    Given the i and j coordinates of the pixel of the image img, return an image with img drawn with a red point 
+    '''
+    # Draw a point at center (i,j)
+    for index in range(len(coordlist)): 
+        i, j = coordlist[index]
+        cv.circle(img, (int(j*scale),int(i*scale)), 5, (int(color[index][0]), int(color[index][1]), int(color[index][2])), 2)
+    return img 
+
+def calculateNewPosition(coordlist, flow): 
+    ''' 
+    In: 
+    coordlist: List of annotated points coordlist [(i1, j1), (i2, j2), ..., (in, jn)] 
+    to calculate new positions in the next frame. 
+    flow: vector field of the movements of each pixel 
+    Out: 
+    newcoordlist: list of pseudo labeled points in the next frame (if they are still in frame). 
+    Length of the list is inferior or equal to the input list. 
+    inFrame: list of booleans to say if the annotated point is in frame. 
+    '''
+    newcoordlist, inFrame = [], []
+    framewidth = flow.shape[1]
+    frameheight = flow.shape[0]
+    for coord in coordlist: 
+        i, j = coord
+        newi, newj = i + flow[i, j, 0], j + flow[i, j, 1]
+        if newi < frameheight and newj < framewidth and newi > 0 and newj > 0: 
+            newcoordlist.append((int(newi), int(newj)))
+            inFrame.append(True)
+        else: 
+            inFrame.append(False)
+    
+    return newcoordlist, inFrame

25cf0558e371146bd4056e6d602bd610e6013d21
mirarzf <mira.razaf@gmail.com>
2022-11-09 11:22:36 +0900

    Updated read me

--- flowcomparison.py
+++ flowcomparison.py
@@ -216,7 +216,7 @@ def compareToAnnotatedPointsFlow(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="This script creates a video containing all the resulting map of attention "
                                                 "to focus the segmentation map around the hand")
-    parser.add_argument('--model', default='raft-sintel.pth', help="restore checkpoint")
+    parser.add_argument('--model', default='raft-things.pth', help="restore checkpoint")
     parser.add_argument('--dataset', default='C:\\Users\\hvrl\\Documents\\data\\KU\\centerpoints.csv', help="CSV file with annotated points")
     parser.add_argument('--videofolder', '-vf', default='C:\\Users\\hvrl\\Documents\\data\\KU\\videos', help="folder containig the annotated videos")
     parser.add_argument('--scale', default=0.5, type=float, help="scale to resize the video frames. Default: 0.5")

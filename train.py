aa41c670e6d550e28563ace3232bf0782459a05f
mirarzf <mira.razaf@gmail.com>
2022-11-22 13:26:47 +0900

    Updated gitignore

--- train.py
+++ train.py
@@ -11,8 +11,8 @@ from torch import optim
 from torch.utils.data import DataLoader, random_split
 from tqdm import tqdm
 
-from utils.data_loading import BasicDataset, CarvanaDataset
-from utils.dice_score import dice_loss
+from unetutils.data_loading import BasicDataset, CarvanaDataset
+from unetutils.dice_score import dice_loss
 from evaluate import evaluate
 from unet import UNet
 

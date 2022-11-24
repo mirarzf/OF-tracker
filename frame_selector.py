c6715382ecf6843bf4dde216a288d0c6363aab4c
mirarzf <mira.razaf@gmail.com>
2022-11-24 18:01:48 +0900

    Corrected frame_selector

--- frame_selector.py
+++ frame_selector.py
@@ -2,11 +2,21 @@ import os
 import shutil 
 
 framesfolder = 'D:\\Master Thesis\\data\\KU\\test'
-toKeepFolder = './extracted_frames/to_keep/'
+effolder = '.\\extracted_frames'
+toKeepFolder = os.path.join(effolder, 'to_keep')
 if not os.path.exists(toKeepFolder): 
     os.mkdir(toKeepFolder)
+print(toKeepFolder)
 
 filenames = [os.path.basename(f) for f in os.listdir(framesfolder)]
-for filename in filenames: 
-    cdfilename = os.path.join('/extracted_frames', filename)
-    print(shutil.move(cdfilename, toKeepFolder), filename)
+videodirs = [os.path.join(effolder, dir) for dir in os.listdir(effolder) if 'to_keep' not in dir]
+print(videodirs)
+for dir in videodirs: 
+    print(dir)
+    vidfilenames = [os.path.basename(f) for f in os.listdir(dir)]
+    for filename in vidfilenames: 
+        if filename in filenames: 
+            cdfilename = os.path.join(dir, filename)
+            destfilename = os.path.join(toKeepFolder, filename)
+            shutil.copyfile(cdfilename, destfilename)
+            print(filename)

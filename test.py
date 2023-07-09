735fe4671438245b421ac7da2b16fbc3171ea4fc
mirarzf <mira.razaf@gmail.com>
2023-07-09 23:35:33 +0900

    Updated test to test more easily

--- test.py
+++ test.py
@@ -89,6 +89,41 @@ def test_net(net,
     # 1. Create dataset
     if len(img_ids) == 0: 
         ids = [file.stem for file in images_dir.iterdir() if file.is_file() and file.name != '.gitkeep']
+        ### SELECT IDs FOR SEQUENCE TRAINING ### 
+        all_ids = [file.stem for file in images_dir.iterdir() if file.is_file() and file.name != '.gitkeep']
+        test_id = "0838_0917"
+        # test_id = "2108_2112"
+        # test_id = "5909_5915"
+        # test_id = "green0410_0452"
+        # test_id = "green0810_0840"
+        ids = [id for id in all_ids if test_id in id]
+        ### END OF SELECT IDs FOR SEQUENCE TRAINING ###
+        # ### SELECT IDs FOR VIDEO SPLIT ### 
+        # all_ids = [file.stem for file in images_dir.iterdir() if file.is_file() and file.name != '.gitkeep']
+        # test_id_list = ["0838_0917", "2108_2112", "5909_5915"]
+        # # test_id_list = ["green0410_0452", "green0810_0840"]
+        # ids = []
+        # for test_id in test_id_list: 
+        #     ids += [id for id in all_ids if test_id in id]
+        # ### END OF SELECT IDs FOR VIDEO SPLIT ### 
+        # ### SELECT IDs FOR HAND PICKED VALIDATION SET ### 
+        # all_ids = [file.stem for file in images_dir.iterdir() if file.is_file() and file.name != '.gitkeep']
+        # test_id_list = ["0838_0917_extract_10", 
+        #          "0838_0917_extract_100", 
+        #          "0838_0917_extract_400", 
+        #          "2108_2112_extract_110",
+        #          "5909_5915_extract_10", 
+        #          "5909_5915_extract_70", 
+        #          "5909_5915_extract_140", 
+        #          "green0410_0452_extract_750", 
+        #          "green0410_0452_extract_800", 
+        #          "green0410_0452_extract_1000", 
+        #          "green0810_0840_extract_10", 
+        #          "green0810_0840_extract_100"]
+        # ids = []
+        # for test_id in test_id_list: 
+        #     ids += [id for id in all_ids if test_id in id]
+        # ### END OF SELECT IDs FOR HAND PICKED VALIDATION SET ### 
     else: 
         ids = img_ids 
     attmapdirForTest = '' if attmaps_dir == None else attmaps_dir

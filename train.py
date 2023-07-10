41652f9ad7ae0718001f5ade39f7511d0e0af3c0
mirarzf <mira.razaf@gmail.com>
2023-07-10 15:18:14 +0900

    Updated diagram changed train.py for RS 100 epochs

--- train.py
+++ train.py
@@ -124,7 +124,7 @@ def train_net(net,
         val_ids = [] 
     ### SELECT IDs FOR SEQUENCE TRAINING ### 
     # test_id = "0838_0917"
-    test_id = "2108_2112"
+    # test_id = "2108_2112"
     # test_id = "5909_5915"
     # test_id = "green0410_0452"
     # test_id = "green0810_0840"
@@ -144,21 +144,21 @@ def train_net(net,
     #     val_ids += [id for id in ids if test_id in id]
     # train_ids = [id for id in ids if id not in val_ids]
     # ### END OF SELECT IDs FOR VIDEO SPLIT ### 
-    # ### SELECT IDs FOR HAND PICKED VALIDATION SET ### 
-    # val_ids = ["0838_0917_extract_10", 
-    #          "0838_0917_extract_100", 
-    #          "0838_0917_extract_400", 
-    #          "2108_2112_extract_110",
-    #          "5909_5915_extract_10", 
-    #          "5909_5915_extract_70", 
-    #          "5909_5915_extract_140", 
-    #          "green0410_0452_extract_750", 
-    #          "green0410_0452_extract_800", 
-    #          "green0410_0452_extract_1000", 
-    #          "green0810_0840_extract_10", 
-    #          "green0810_0840_extract_100"]
-    # train_ids = [id for id in ids if id not in val_ids]
-    # ### END OF SELECT IDs FOR HAND PICKED VALIDATION SET ### 
+    ### SELECT IDs FOR HAND PICKED VALIDATION SET ### 
+    val_ids = ["0838_0917_extract_10", 
+             "0838_0917_extract_100", 
+             "0838_0917_extract_400", 
+             "2108_2112_extract_110",
+             "5909_5915_extract_10", 
+             "5909_5915_extract_70", 
+             "5909_5915_extract_140", 
+             "green0410_0452_extract_750", 
+             "green0410_0452_extract_800", 
+             "green0410_0452_extract_1000", 
+             "green0810_0840_extract_10", 
+             "green0810_0840_extract_100"]
+    train_ids = [id for id in ids if id not in val_ids]
+    ### END OF SELECT IDs FOR HAND PICKED VALIDATION SET ### 
     n_train = len(train_ids)
     n_val = len(val_ids)
     val_percent = round(n_val/n_train,2) 

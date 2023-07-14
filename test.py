f74dfb832038625907ccb201e7e3728f294d40c4
mirarzf <mira.razaf@gmail.com>
2023-07-14 20:56:58 +0900

    Updated notebook for overlayed figures

--- test.py
+++ test.py
@@ -89,14 +89,14 @@ def test_net(net,
     # 1. Create dataset
     if len(img_ids) == 0: 
         ids = [file.stem for file in images_dir.iterdir() if file.is_file() and file.name != '.gitkeep']
-        ### SELECT IDs FOR SEQUENCE TRAINING ### 
-        all_ids = [file.stem for file in images_dir.iterdir() if file.is_file() and file.name != '.gitkeep']
-        test_id = "0838_0917"
-        # test_id = "2108_2112"
-        # test_id = "5909_5915"
-        # test_id = "green0410_0452"
-        # test_id = "green0810_0840"
-        ids = [id for id in all_ids if test_id in id]
+        # ### SELECT IDs FOR SEQUENCE TRAINING ### 
+        # all_ids = [file.stem for file in images_dir.iterdir() if file.is_file() and file.name != '.gitkeep']
+        # test_id = "0838_0917"
+        # # test_id = "2108_2112"
+        # # test_id = "5909_5915"
+        # # test_id = "green0410_0452"
+        # # test_id = "green0810_0840"
+        # ids = [id for id in all_ids if test_id in id]
         ### END OF SELECT IDs FOR SEQUENCE TRAINING ###
         # ### SELECT IDs FOR VIDEO SPLIT ### 
         # all_ids = [file.stem for file in images_dir.iterdir() if file.is_file() and file.name != '.gitkeep']
